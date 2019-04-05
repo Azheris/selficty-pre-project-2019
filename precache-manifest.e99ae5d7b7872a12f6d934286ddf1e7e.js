c6cc308554b2bf925b0c2678265443b650e67c32
Guilhem Amardeilh <guilhem.amardeilh@epitech.eu>
2019-04-05 16:51:04 +0200

    Updates

---
+++ precache-manifest.e99ae5d7b7872a12f6d934286ddf1e7e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216a5ff65563666ac3",
+    "url": "/selfcity-pre-project-2019/static/css/main.3184edbc.chunk.css"
+  },
+  {
+    "revision": "b6216a5ff65563666ac3",
+    "url": "/selfcity-pre-project-2019/static/js/main.4f060131.chunk.js"
+  },
+  {
+    "revision": "b740b5f27cfc9eb6536b",
+    "url": "/selfcity-pre-project-2019/static/js/runtime~main.97be628c.js"
+  },
+  {
+    "revision": "cd51348a6a67b672f2b4",
+    "url": "/selfcity-pre-project-2019/static/js/2.21ceb7af.chunk.js"
+  },
+  {
+    "revision": "5bd6c66f1bc315d823464ebfba8dcfee",
+    "url": "/selfcity-pre-project-2019/index.html"
+  }
+];
